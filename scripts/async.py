6e1c7bb2bd80b9e2c1d282973f4f5294ed0fd9e8
Kytra Selca <kytrascript@gmail.com>
2023-07-12 19:24:15 -0500

    corrected async counter

--- scripts/async.py
+++ scripts/async.py
@@ -3,6 +3,8 @@ import asyncio
 async def main():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://6tdnw9xx0l.execute-api.us-east-2.amazonaws.com/default/animatediffcount') as resp:
-            print(resp.status)
-            print(await resp.text())
-await main()
+            if resp.status == 200:
+                print("Count Successful")
+
+if __name__ == "__main__":
+    asyncio.run(main())
