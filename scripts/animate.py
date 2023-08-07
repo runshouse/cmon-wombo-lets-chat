06a91e24053ca6fe5bbbdfd01ea3a9085e69cb2d
Runbo Li <runboli2117@gmail.com>
2023-08-07 16:31:50 -0400

    Update animate.py

--- scripts/animate.py
+++ scripts/animate.py
@@ -63,7 +63,7 @@ def main(args):
             tokenizer = CLIPTokenizer.from_pretrained(args.pretrained_model_path, subfolder="tokenizer", torch_dtype=torch.float16, variant="fp16")
             text_encoder = CLIPTextModel.from_pretrained(args.pretrained_model_path, subfolder="text_encoder", torch_dtype=torch.float16, variant="fp16")
             vae = AutoencoderKL.from_pretrained(args.pretrained_model_path, subfolder="vae", torch_dtype=torch.float16, variant="fp16")
-            unet = UNet3DConditionModel.from_pretrained_2d(args.pretrained_model_path, subfolder="unet", torch_dtype=torch.float16, variant="fp16"
+            unet = UNet3DConditionModel.from_pretrained_2d(args.pretrained_model_path, subfolder="unet", torch_dtype=torch.float16, variant="fp16",
                                                            unet_additional_kwargs=OmegaConf.to_container(
                                                                inference_config.unet_additional_kwargs))
             print('Made it to line 68')
